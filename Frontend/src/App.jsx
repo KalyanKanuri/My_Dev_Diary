e67000e794897144470340697c3446e6f1fcb639
Kalyan.Kanuri <kalyankanuri497@gmail.com>
2024-07-04 21:36:54 +0530

    Add routing to Header

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from 'react'
-import APIHandler from './lib/APIHandler'
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import APIHandler from './lib/APIHandler';
 import Header from './components/Header';
-import Home from './pages/Home'
+import Home from './pages/Home';
+import Education from './pages/Education';
+import Experience from './pages/Experience';
+import Projects from './pages/Projects';
+import Contact from "./pages/ContactMe";
 
 function App() {
   const [message, setMessage] = useState('');
@@ -19,17 +24,22 @@ function App() {
   }, []);
   
   return (
-    <>
-      {/* Other components and content */}
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <div className="toast">
         <div className="alert alert-info">
           <span>{message}</span>
         </div>
       </div>      
-    </>
+    </Router>
   );
 }
   
-export default App
+export default App;
